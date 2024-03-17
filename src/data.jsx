4720185217ee8e10369b72167f1a08c709a4ec9b
Krystian <texen24@gmail.com>
2024-03-17 11:44:34 +0100

    implemented logic responsilbe for check who is the winner

--- src/data.jsx
+++ src/data.jsx
@@ -1,24 +1,53 @@
-import {
-  IoSettingsOutline,
-  IoInformationCircleOutline,
-  IoBowlingBallOutline,
-} from 'react-icons/io5';
-import { CgUndo } from 'react-icons/cg';
-import { HiOutlineBookOpen } from 'react-icons/hi';
-import { BsClockHistory } from 'react-icons/bs';
-
-export const icons = [
-  { id: 'new-game', icon: <IoBowlingBallOutline /> },
-  { id: 'undo', icon: <CgUndo /> },
-
-  { id: 'settings', icon: <IoSettingsOutline /> },
-  {
-    id: 'about',
-    icon: <IoInformationCircleOutline />,
-  },
-
-  { id: 'rules', icon: <HiOutlineBookOpen /> },
-  { id: 'history', icon: <BsClockHistory /> },
-];
-
-
+export const sortSlots = (array) => {
+  const sortedBoard = [
+    [
+      { a1: array[0][0] },
+      { a2: array[0][1] },
+      { a3: array[0][2] },
+      { b1: array[0][3] },
+      { b2: array[0][4] },
+      { b3: array[0][5] },
+      { c1: array[0][6] },
+      { c2: array[0][7] },
+      { c3: array[0][8] },
+    ],
+    [
+      { a4: array[1][0] },
+      { a5: array[1][1] },
+      { a6: array[1][2] },
+      { b4: array[1][3] },
+      { b5: array[1][4] },
+      { b6: array[1][5] },
+      { c4: array[1][6] },
+      { c5: array[1][7] },
+      { c6: array[1][8] },
+    ],
+    [
+      { d1: array[2][0] },
+      { d2: array[2][1] },
+      { d3: array[2][2] },
+      { e1: array[2][3] },
+      { e2: array[2][4] },
+      { e3: array[2][5] },
+      { f1: array[2][6] },
+      { f2: array[2][7] },
+      { f3: array[2][8] },
+    ],
+    [
+      { d4: array[3][0] },
+      { d5: array[3][1] },
+      { d6: array[3][2] },
+      { e4: array[3][3] },
+      { e5: array[3][4] },
+      { e6: array[3][5] },
+      { f4: array[3][6] },
+      { f5: array[3][7] },
+      { f6: array[3][8] },
+    ],
+  ];
+  return sortedBoard.flat().sort((a, b) => {
+    const keyA = Object.keys(a)[0];
+    const keyB = Object.keys(b)[0];
+    return keyA.localeCompare(keyB);
+  });
+};
--- src/gameContext.jsx
+++ src/gameContext.jsx
@@ -1,7 +1,7 @@
 import { useState, createContext, useContext } from 'react';
+import { sortSlots } from './data';
 
 const Context = createContext();
-
 
 const GameContext = ({ children }) => {
   const initialSlots = new Array(9).fill(null);
@@ -12,6 +12,110 @@ const GameContext = ({ children }) => {
     initialSlots,
     initialSlots,
   ]);
+
+  const checkIfWin = (fullBoard) => {
+    const sortedSlots = sortSlots(fullBoard);
+    const players = ['player-one', 'player-two'];
+    
+    for (let player of players) {
+      // Rows
+      for (let i = 0; i < 6; i++) {
+        for (let j = 0; j <= 1; j++) {
+          let count = 0;
+          for (let k = j; k < j + 5; k++) {
+            if (
+              sortedSlots[i * 6 + k] &&
+              sortedSlots[i * 6 + k][Object.keys(sortedSlots[i * 6 + k])[0]] ===
+                player
+            ) {
+              count++;
+            }
+          }
+          if (count === 5) {
+            console.log(
+              'Player',
+              player === 'player-one' ? 'one' : 'two',
+              'wins'
+            );
+            return;
+          }
+        }
+      }
+      // Columns
+      for (let i = 0; i < 6; i++) {
+        for (let j = 0; j <= 1; j++) {
+          let count = 0;
+          for (let k = j; k < j + 5; k++) {
+            if (
+              sortedSlots[k * 6 + i] &&
+              sortedSlots[k * 6 + i][Object.keys(sortedSlots[k * 6 + i])[0]] ===
+                player
+            ) {
+              count++;
+            }
+          }
+          if (count === 5) {
+            console.log(
+              'Player',
+              player === 'player-one' ? 'one' : 'two',
+              'wins'
+            );
+            return;
+          }
+        }
+      }
+      // Diagonals
+      for (let i = 0; i <= 1; i++) {
+        for (let j = 0; j <= 1; j++) {
+          let count = 0;
+          for (let k = 0; k < 5; k++) {
+            if (
+              sortedSlots[(i + k) * 6 + j + k] &&
+              sortedSlots[(i + k) * 6 + j + k][
+                Object.keys(sortedSlots[(i + k) * 6 + j + k])[0]
+              ] === player
+            ) {
+              count++;
+            }
+          }
+          if (count === 5) {
+            console.log(
+              'Player',
+              player === 'player-one' ? 'one' : 'two',
+              'wins'
+            );
+            return;
+          }
+        }
+      }
+      // Reverse diagonals
+      for (let i = 0; i <= 1; i++) {
+        for (let j = 4; j < 6; j++) {
+          let count = 0;
+          for (let k = 0; k < 5; k++) {
+            if (
+              sortedSlots[(i + k) * 6 + j - k] &&
+              sortedSlots[(i + k) * 6 + j - k][
+                Object.keys(sortedSlots[(i + k) * 6 + j - k])[0]
+              ] === player
+            ) {
+              count++;
+            }
+          }
+          if (count === 5) {
+            console.log(
+              'Player',
+              player === 'player-one' ? 'one' : 'two',
+              'wins'
+            );
+            return;
+          }
+        }
+      }
+    }
+  };
+
+  checkIfWin(fullBoard);
 
   return (
     <Context.Provider
